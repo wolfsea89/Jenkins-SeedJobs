310a957c4f271c75e0653ae5e9c765d4596f7fd1
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-22 23:35:52 +0100

    test

--- seedJob.groovy
+++ seedJob.groovy
@@ -6,6 +6,6 @@ def jobs = new JsonSlurper().parseText(readFileFromWorkspace(jobsDefinition))
 
 for (job in jobs){
   if(job.jobType == "Build"){
-    evaluate(new File("./BuildsJobs/buildJobs.groovy"))
+    evaluate(readFileFromWorkspace("BuildsJobs/buildJobs.groovy"))
   }
 }
