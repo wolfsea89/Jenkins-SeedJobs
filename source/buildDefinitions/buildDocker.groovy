13cf98a997b57a18547db0f5c3ab78760d711eaf
Maciej Rachuna <rachuna.maciej@gmail.com>
2021-02-24 20:38:30 +0100

    Create SeedJobs (#4)
    Create infrastructure #1
    Create first job in jenkins #2
    Create first view in jenkins #3

---
+++ source/buildDefinitions/buildDocker.groovy
@@ -0,0 +1,67 @@
+import groovy.json.*
+
+String jobsDefinition = 'Definitions/jobs.json'
+
+def jobsList = new JsonSlurper().parseText(readFileFromWorkspace(jobsDefinition))
+
+for (jobItem in jobsList){
+  if(jobItem.jobType == "BuildDocker"){
+        def publishJson = new JsonSlurper().parseText("""
+      {
+        "DockerHubRelease": {
+          "repositoryName": "wolfsea89/${jobItem.docker.name}",
+          "repositoryCredentialID": "docker_hub",
+          "repositoryUrl": "https://index.docker.io/v1/"
+        },
+        "DockerHubSnapshot": {
+          "repositoryName": "wolfsea89/${jobItem.docker.name}-snapshot",
+          "repositoryCredentialID": "docker_hub",
+          "repositoryUrl": "https://index.docker.io/v1/"
+        },
+        "GitHubRelease": {
+          "repositoryName": "docker.pkg.github.com/wolfsea89/jenkins-baseimage/${jobItem.docker.name}",
+          "repositoryCredentialID": "github",
+          "repositoryUrl": "https://docker.pkg.github.com/"
+        }
+      }
+      """)
+
+    pipelineJob(jobItem.name) {
+      parameters{
+        stringParam("branchName", jobItem.defaultBranch, 'Branch name')
+        stringParam("repositoryUrl", jobItem.repositoryUrl, 'Repository URL (git/https)')
+        stringParam("manualVersion", "", 'Set manual version (X.Y.Z). Worked with branch release, hotfix, master without version')
+      }
+      environmentVariables {
+        env('JENKINSFILE_SCRIPTS_DIR', '.jenkins')
+        env('GIT_CREDS_ID', 'github')
+        env('APP_CONFIGURATION_JSON_PATH', 'configuration/jenkins.json')
+        env('BASEIMAGE_SERVICES_ADMIN_CREDS_ID', 'baseImage_services_AminPassword')
+        env('PUBLISH_REPOSITORIES', JsonOutput.toJson(publishJson))
+      }
+      definition {
+        cpsScm{
+          scm {
+            git {
+              remote {
+                  url('git@github.com:wolfsea89/Jenkins-Ci-Jenkinsfiles.git')
+                  credentials('github')
+              }
+              branch('feature/seedjob')
+              extensions {
+                wipeOutWorkspace()
+                submoduleOptions {
+                  disable(false)
+                  parentCredentials(true)
+                  recursive(false)
+                  tracking(true)
+                }
+              }
+            }
+            scriptPath('Jenkinsfile')
+          }
+        }
+      }
+    }
+  }
+}
---
+++ source/seedJob.groovy
@@ -0,0 +1,33 @@
+def seedJobs = [
+  'source/seedJob.groovy',
+  'source/view/dockerBuilder.groovy',
+  'source/buildDefinitions/buildDocker.groovy'
+]
+
+String targetsSeedJobs
+for (item in seedJobs){
+  if(targetsSeedJobs == null){
+    targetsSeedJobs = item + '\n'
+  } else {
+    targetsSeedJobs += item + '\n'
+  }
+}
+
+job('seedJob'){
+  displayName('seedJob')
+  description('Create jobs')
+  scm {
+    git {
+      remote {
+        url('git@github.com:wolfsea89/Jenkins-SeedJobs.git')
+        credentials('github')
+      }
+      branch('feature/create_seedjob')
+    }
+  }
+  steps {
+    jobDsl {
+      targets(targetsSeedJobs)
+    }
+  }
+}
